f33be3e2d4358b29dfffcec2160ee86d21b27ee7
angellom <angellom@jfrog.com>
2018-05-11 15:31:35 -0700

    Population methods now working correctly for both naturals and triplets

--- sumofsquares/dbtools.py
+++ sumofsquares/dbtools.py
@@ -4,8 +4,10 @@
 """ Functions and classes to manage DB tasks such as population
 """
 from sumofsquares.database import init_db
-from sumofsquares.models import Natural
+from sumofsquares.models import Natural, Triplet
 from sumofsquares.database import db_session
+from sumofsquares import app
+import math
 
 
 class DBHandler(object):
@@ -14,15 +16,24 @@ class DBHandler(object):
         self.mode = mode
 
     def populate(self, n: int) -> int:
+        """ Triggers both populations; naturals and triplets."""
+        if n > app.config["POPULATE_MAX"]:
+            return 0
+        self.populate_natural(n)
+        self.populate_triplet(n)
+
+    def populate_natural(self, n: int) -> int:
         """ Populates database to work with the first n Natural numbers """
-        if self.mode != 'w':
+        if self.mode != 'w' or not isinstance(n, int):
+            return 0
+        if n > app.config["POPULATE_MAX"]:
             return 0
 
         init_db()
 
         current_termial = 0
         current_ss = 0
-        for i in range(1, n):
+        for i in range(1, n+1):
 
             ss = i**2 + current_ss
             current_ss = ss
@@ -36,6 +47,25 @@ class DBHandler(object):
 
             db_session.add(natural)
             db_session.commit()
+        return 1
+
+    def populate_triplet(self, n: int) -> int:
+        """ Will populate triplet DB"""
+
+        if self.mode != 'w' or not isinstance(n, int):
+            return 0
+        if n > app.config["POPULATE_MAX"]:
+            return 0
+
+        init_db()
+
+        triplets = Triplet.gen_all_pyth_trips(n)
+        for triplet in triplets:
+            a, b, c = int(triplet[0]), int(triplet[1]), int(triplet[2])
+            new = Triplet(min(a, b), max(a, b), c, a*b*c)
+            db_session.add(new)
+            db_session.commit()
+
         return 1
 
 
