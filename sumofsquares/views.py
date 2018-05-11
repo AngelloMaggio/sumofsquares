f068455cb38856e9a70617c72db8f22e52103189
angellom <angellom@jfrog.com>
2018-05-11 13:44:49 -0700

    Added /triplets endpoint. Improved error handling, adjusted imports to bring in new model

--- sumofsquares/views.py
+++ sumofsquares/views.py
@@ -5,7 +5,7 @@
 """
 
 from sumofsquares import app
-from sumofsquares.models import Natural
+from sumofsquares.models import Natural, Triplet
 from sumofsquares.database import init_db
 from sumofsquares.dbtools import DBHandler
 from flask import jsonify, request
@@ -24,6 +24,9 @@ def ss_termial_diff():
 }
     """
     n = request.args.get('n', type=int)
+    print(n)
+    if not n:
+        return jsonify({'Message': "Input value is not an integer."})
 
     init_db()
 
@@ -32,14 +35,31 @@ def ss_termial_diff():
     try:
         answer = natural.termial_sq - natural.sumofsquares
     except (AttributeError, NameError) as err:
-        if not n:
-            return jsonify({'Message': "Input value is not an integer."})
         return jsonify({'Message': "Input value {} is either not natural or out of range.".format(n)})
 
     response = {'datetime': datetime.datetime.now(), 'value': answer, 'number': n, 'occurences': natural.request_count,
                 'last_datetime': natural.last_request}
 
     return jsonify(response)
+
+
+@app.route('/triplets', methods=['GET'])
+def triplets():
+    """ Returns whether 3 numbers are a pythagorean triplet and if their product is equals to n"""
+
+    a = request.args.get("a", type=int)
+    b = request.args.get("b", type=int)
+    c = request.args.get("c", type=int)
+    n = request.args.get("n", type=int)
+
+    if None in (a, b, c, n):
+        return jsonify({'Message': "One or more your input values is not an integer."})
+
+    if max(a, b, c) != c:
+        return jsonify({'Value': "False"})
+
+    if Triplet.is_pythagorean_triplet(a, b, c, n):
+        return jsonify({'Value': "True"})
 
 
 @app.route('/populate/<int:n>', methods=['GET'])
