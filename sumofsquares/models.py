8202cf4df773e151a8c37fdb56c54a45bd7155ba
angellom <angellom@jfrog.com>
2018-05-11 13:48:44 -0700

    Added new model to store triplets and relevant methods for quick access and future implementations.

--- sumofsquares/models.py
+++ sumofsquares/models.py
@@ -7,6 +7,7 @@
 from sqlalchemy import Column, Integer, DateTime
 from sumofsquares.database import Base
 import datetime
+import numpy as np
 
 
 class Natural(Base):
@@ -19,8 +20,9 @@ class Natural(Base):
     request_count = Column(Integer)
     last_request = Column(DateTime)
 
-    def __init__(self, n, termial=None, termial_sq=None, sumofsquares=None, request_count=0):
+    def __init__(self, n, square=None, termial=None, termial_sq=None, sumofsquares=None, request_count=0):
         self.n = n
+        self.square = squares
         self.termial = termial
         self.termial_sq = termial_sq
         self.sumofsquares = sumofsquares
@@ -32,6 +34,8 @@ class Natural(Base):
 
     def construct(self):
         """ Set values depending solely on n being set """
+        if not self.square:
+            self.square = self.n**2
         if not self.termial:
             self.termial = self.get_termial(self.n)
         if not self.termial_sq:
@@ -48,3 +52,54 @@ class Natural(Base):
     def get_ss(n: int) -> int:
         """ Returns the sum of the squares of the first n natural numbers """
         return n**3/3 + n**2/2 + n/6
+
+
+class Triplet(Base):
+    """ Store triplets for easy querying"""
+    __tablename__ = 'triplets'
+
+    n = Column(Integer, primary_key=True)
+    a = Column(Integer)
+    b = Column(Integer)
+    c = Column(Integer)
+    last_request = Column(DateTime)
+
+    def __init__(self, a, b, c, n=None):
+        self.a = a
+        self.b = b
+        self.c = c
+        if not n:
+            self.n = a*b*c
+
+    @staticmethod
+    def is_pythagorean_triplet(a: int, b: int, c: int, n=None) -> bool:
+        """ Simple way of checking if a,b,c are a pythagorean triplet and if their product equals to n (n given)"""
+        if not n:
+            return a**2 + b**2 == c**2
+        return (a**2 + b**2 == c**2) and a*b*c == n
+
+    @staticmethod
+    def gen_prim_pyth_trips(limit):
+        """ Finds all primitive Pythagorean triplets up to limit
+        Sourced from Stackoverflow
+        https://stackoverflow.com/questions/575117/generating-unique-ordered-pythagorean-triplets/"""
+        u = np.mat(' 1  2  2; -2 -1 -2; 2 2 3')
+        a = np.mat(' 1  2  2;  2  1  2; 2 2 3')
+        d = np.mat('-1 -2 -2;  2  1  2; 2 2 3')
+        uad = np.array([u, a, d])
+        m = np.array([3, 4, 5])
+        while m.size:
+            m = m.reshape(-1, 3)
+            if limit:
+                m = m[m[:, 2] <= limit]
+            yield from m
+            m = np.dot(m, uad)
+
+    @staticmethod
+    def gen_all_pyth_trips(limit):
+        """ Finds all Pythagorean triplets up to limit"""
+        for prim in Triplet.gen_prim_pyth_trips(limit):
+            i = prim
+            for _ in range(limit//prim[2]):
+                yield i
+                i = i + prim
